47671407fe9bc8e61dd96f163ad9e068db678c90
JerryHorton <2440021574@qq.com>
2024-12-19 23:26:13 +0800

    add comments

--- core/nty_schedule.c
+++ core/nty_schedule.c
@@ -124,7 +124,7 @@ void nty_schedule_desched_sleepdown(nty_coroutine *co) {
 nty_coroutine *nty_schedule_search_wait(int fd) {
     nty_coroutine find_it = {0};  // 构造一个虚拟协程用于搜索目标协程
     find_it.fd = fd;
-    nty_schedule *sched = nty_coroutine_get_sched();  // 获取当前线程的调度器
+    nty_schedule * sched = nty_coroutine_get_sched();  // 获取当前线程的调度器
     nty_coroutine *co = RB_FIND(_nty_coroutine_rbtree_wait, &sched->waiting, &find_it);  // 查找相关协程
     if (co == NULL) {  // 未找到匹配协程
         return NULL;
@@ -144,7 +144,7 @@ void nty_schedule_sched_wait(nty_coroutine *co, int fd, unsigned short events, u
         " already in %"
         PRId32
         " state\n",
-        co->id, co->fd, co->status);
+                co->id, co->fd, co->status);
         assert(0);  // 强制触发程序崩溃
     }
     if (events & POLLIN) {  // 等待可读事件
@@ -179,105 +179,98 @@ nty_coroutine *nty_schedule_desched_wait(int fd) {
     return co;
 }
 
+/* 从调度器的等待队列中移除指定的协程 */
 void nty_schedule_cancel_wait(nty_coroutine *co) {
     RB_REMOVE(_nty_coroutine_rbtree_wait, &co->sched->waiting, co);
 }
 
+/* 清理和释放调度器资源 */
 void nty_schedule_free(nty_schedule *sched) {
-    if (sched->poller_fd > 0) {
+    if (sched->poller_fd > 0) {  // 关闭 poller 文件描述符
         close(sched->poller_fd);
     }
-    if (sched->eventfd > 0) {
+    if (sched->eventfd > 0) {  // 关闭 eventfd 文件描述符
         close(sched->eventfd);
     }
-    if (sched->stack != NULL) {
+    if (sched->stack != NULL) {  // 释放调度器栈空间
         free(sched->stack);
     }
-
-    free(sched);
-
-    assert(pthread_setspecific(global_sched_key, NULL) == 0);
+    free(sched);  // 释放调度器本身的内存
+    assert(pthread_setspecific(global_sched_key, NULL) == 0);  // 清除线程本地变量中保存的调度器
 }
 
+/* 初始化协程调度器 */
 int nty_schedule_create(int stack_size) {
-
-    int sched_stack_size = stack_size ? stack_size : NTY_CO_MAX_STACKSIZE;
-
-    nty_schedule * sched = (nty_schedule *) calloc(1, sizeof(nty_schedule));
-    if (sched == NULL) {
+    int sched_stack_size = stack_size ? stack_size : NTY_CO_MAX_STACKSIZE;  // 设置协程调度器的堆栈大小
+    nty_schedule *sched = (nty_schedule *) calloc(1, sizeof(nty_schedule));  // 为调度器分配内存
+    if (sched == NULL) {  // 分配失败
         printf("Failed to initialize scheduler\n");
         return -1;
     }
-
-    assert(pthread_setspecific(global_sched_key, sched) == 0);
-
-    sched->poller_fd = nty_epoller_create();
-    if (sched->poller_fd == -1) {
+    assert(pthread_setspecific(global_sched_key, sched) == 0);  // 线程局部存储（TLS）初始化，将调度器设置为当前线程的全局变量
+    sched->poller_fd = nty_epoller_create();  // epoll 初始化
+    if (sched->poller_fd == -1) {  // 失败则调用 nty_schedule_free 清理已经分配的资源
         printf("Failed to initialize epoller\n");
         nty_schedule_free(sched);
         return -2;
     }
+    nty_epoller_ev_register_trigger();  // 注册与事件触发相关的资源
+    sched->stack_size = sched_stack_size;  // 设置调度器堆栈大小
+    sched->page_size = getpagesize();  // 设置页面大小
 
-    nty_epoller_ev_register_trigger();
-
-    sched->stack_size = sched_stack_size;
-    sched->page_size = getpagesize();
-
-#ifdef _USE_UCONTEXT
-    int ret = posix_memalign(&sched->stack, sched->page_size, sched->stack_size);
-    assert(ret == 0);
-#else
+#ifdef _USE_UCONTEXT  // 基于 ucontext 的协程切换实现需要栈空间来保存协程的上下文状态
+    int ret = posix_memalign(&sched->stack, sched->page_size, sched->stack_size);  // 为调度器分配一块对齐到页面大小的内存作为协程的栈空间
+    assert(ret == 0);  // 分配成功，返回值为 0
+#else  // 基于汇编手动实现的协程切换不需要专门的栈分配
     sched->stack = NULL;
     bzero(&sched->ctx, sizeof(nty_cpu_ctx));
 #endif
 
-    sched->spawned_coroutines = 0;
-    sched->default_timeout = 3000000u;
-
-    RB_INIT(&sched->sleeping);
-    RB_INIT(&sched->waiting);
-
-    sched->birth = nty_coroutine_usec_now();
-
-    TAILQ_INIT(&sched->ready);
-    TAILQ_INIT(&sched->defer);
-    LIST_INIT(&sched->busy);
-
+    sched->spawned_coroutines = 0;  // 初始化调度器中已生成的协程计数
+    sched->default_timeout = 3000000u;  // 设置调度器的默认超时时间
+    RB_INIT(&sched->sleeping);  // 初始化睡眠队列
+    RB_INIT(&sched->waiting);  // 初始化等待队列
+    sched->birth = nty_coroutine_usec_now();  // 记录调度器的创建时间
+    TAILQ_INIT(&sched->ready);  // 初始化就绪队列
+    TAILQ_INIT(&sched->defer);  // 初始化延迟队列
+    LIST_INIT(&sched->busy);  // 初始化执行队列
+    return 0;
 }
 
-
+/* 从调度器的睡眠队列中查找并移除一个超时的协程 */
 static nty_coroutine *nty_schedule_expired(nty_schedule *sched) {
-
-    uint64_t t_diff_usecs = nty_coroutine_diff_usecs(sched->birth, nty_coroutine_usec_now());
-    nty_coroutine *co = RB_MIN(_nty_coroutine_rbtree_sleep, &sched->sleeping);
-    if (co == NULL) return NULL;
-
-    if (co->sleep_usecs <= t_diff_usecs) {
-        RB_REMOVE(_nty_coroutine_rbtree_sleep, &co->sched->sleeping, co);
-        return co;
+    uint64_t t_diff_usecs = nty_coroutine_diff_usecs(sched->birth, nty_coroutine_usec_now());  // 计算调度器的当前时间与其创建时间之间的时间差
+    nty_coroutine *co = RB_MIN(_nty_coroutine_rbtree_sleep, &sched->sleeping);  // 从调度器的睡眠队列中获取睡眠时间最短的协程
+    if (co == NULL) {  // 睡眠队列为空
+        return NULL;
     }
-    return NULL;
+    if (co->sleep_usecs <= t_diff_usecs) {  // 协程已经超时
+        RB_REMOVE(_nty_coroutine_rbtree_sleep, &co->sched->sleeping, co);  // 将超时的协程从睡眠队列中移除
+        return co;  // 返回已超时的协程
+    }
+    return NULL;  // 如果最小值的协程尚未超时，则返回 NULL，表明没有协程需要被唤醒
 }
 
+/* 检查调度器是否完成了所有任务 */
 static inline int nty_schedule_isdone(nty_schedule *sched) {
-    return (RB_EMPTY(&sched->waiting) &&
-            LIST_EMPTY(&sched->busy) &&
-            RB_EMPTY(&sched->sleeping) &&
-            TAILQ_EMPTY(&sched->ready));
+    return (RB_EMPTY(&sched->waiting) &&  // 等待队列为空
+            LIST_EMPTY(&sched->busy) &&  // 执行队列为空
+            RB_EMPTY(&sched->sleeping) &&  // 睡眠队列为空
+            TAILQ_EMPTY(&sched->ready));  // 就绪队列为空
 }
 
+/* 计算调度器中最小的超时时间 */
 static uint64_t nty_schedule_min_timeout(nty_schedule *sched) {
-    uint64_t t_diff_usecs = nty_coroutine_diff_usecs(sched->birth, nty_coroutine_usec_now());
-    uint64_t min = sched->default_timeout;
-
-    nty_coroutine *co = RB_MIN(_nty_coroutine_rbtree_sleep, &sched->sleeping);
-    if (!co) return min;
-
-    min = co->sleep_usecs;
-    if (min > t_diff_usecs) {
-        return min - t_diff_usecs;
+    uint64_t t_diff_usecs = nty_coroutine_diff_usecs(sched->birth, nty_coroutine_usec_now());  // 计算当前时间与调度器的创建时间之间的时间差
+    uint64_t min = sched->default_timeout;  // 初始化为调度器的默认超时时间
+    nty_coroutine *co = RB_MIN(_nty_coroutine_rbtree_sleep, &sched->sleeping);  // // 从调度器的睡眠队列中获取睡眠时间最短的协程
+    if (co == NULL) {  // 睡眠队列为空
+        return min;  // 返回默认的超时时间
     }
-
+    min = co->sleep_usecs;  // 更新为最短休眠协程的唤醒时间
+    if (min > t_diff_usecs) {  // 最短的休眠时间大于当前时间差
+        return min - t_diff_usecs;  // 距离该协程应该唤醒的剩余时间
+    }
     return 0;
 }
 
